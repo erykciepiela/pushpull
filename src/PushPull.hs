c2b6605d10f13025f49822b09b30f6587550bd1d
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2021-01-18 21:42:52 +0100

    cleanup

--- src/PushPull.hs
+++ src/PushPull.hs
@@ -1,5 +1,6 @@
 module PushPull where
 
+import Data.Semigroupoid
 import Data.Functor.Contravariant
 import Data.Functor.Contravariant.Divisible
 import Data.Void
@@ -16,15 +17,11 @@ instance Contravariant (Push ctx) where
   contramap f (Push p) = Push $ \c -> p c . f
 
 instance Divisible (Push ctx) where
-  divide f (Push p1) (Push p2) = Push $ \c a -> let (b1, b2) = f a in do
-    p1 c b1
-    p2 c b2
+  divide f (Push p1) (Push p2) = Push $ \c a -> let (b1, b2) = f a in p1 c b1 >> p2 c b2
   conquer = Push $ const $ const $ return ()
 
 instance Decidable (Push ctx) where
-  choose f (Push p1) (Push p2) = Push $ \c a -> case f a of
-    Left b1 -> p1 c b1
-    Right b2 -> p2 c b2
+  choose f (Push p1) (Push p2) = Push $ \c -> either (p1 c) (p2 c) . f
   lose f = Push $ const $ absurd . f
 
 newtype Pull ctx a = Pull (ctx -> STM a)
@@ -38,9 +35,9 @@ instance Applicative (Pull ctx) where
 
 instance Monad (Pull ctx) where
   return = pure
-  (Pull p) >>= f = Pull $ \c -> do
-    (Pull io) <- f <$> p c
-    io c
+  (Pull pull1) >>= f = Pull $ \c -> do
+    Pull pull2 <- f <$> pull1 c
+    pull2 c
 
 data Cell ctx a b = Cell {
   pushCell :: Push ctx a,
@@ -51,7 +48,46 @@ instance Profunctor (Cell ctx) where
   rmap f (Cell w r) = Cell w (fmap f r)
   lmap f (Cell w r) = Cell (contramap f w) r
 
--- Cell is not a Category (lack of id :: Cell ctx a a) => Cell is not an Arrow
+-- Cell is not a Category (lack of id :: Cell ctx a a) => Cell is not an Arrow either.
+-- At least it's a Semigroupoid:
+-- Still, not sure if it's Cell Semigroupoing is good idea: you're still able to push only to cell1 and cell2 will not be affected
+instance Semigroupoid (Cell ctx) where
+  Cell (Push push2) (Pull pull2) `o` Cell (Push push1) (Pull pull1)  = Cell (Push push1and2) (Pull pull2)
+    where
+      push1and2 c a = do
+        push1 c a
+        b <- pull1 c
+        push2 c b
+-- one might think there's an alternative implementation, but it's invalid:
+-- Cell (Push push2) (Pull pull2) `o` Cell (Push push1) (Pull pull1) = Cell (Push push1) (Pull pull1and2)
+--    where
+--      pull1and2 c = do
+--        b <- pull1 c
+--        push2 c b -- this is wrong: we shoud not push each time we pull!
+--        pull2 c
+
+instance Functor (Cell ctx a) where
+  fmap = rmap
+
+instance Applicative (Cell ctx a) where
+  pure a = Cell conquer (pure a)
+  Cell pushg1 pullf <*> Cell pushg2 pulla = Cell (divide (\a -> (a, a)) pushg1 pushg2) (pullf <*> pulla)
+
+instance Monad (Cell ctx a) where
+  return = pure
+  Cell (Push push1) (Pull pull1) >>= f = let
+    push c a = do
+      push1 c a
+      b <- pull1 c
+      let Cell (Push push2) _ = f b
+      push2 c a
+    pull c = do
+      b <- pull1 c
+      let Cell _ (Pull pull2) = f b
+      pull2 c
+    in Cell (Push push) (Pull pull)
+
+-- Synonyms in FRP vocabulary
 
 insert :: (b -> a) -> Push ctx a -> Push ctx b
 insert = contramap
@@ -151,6 +187,14 @@ latest = atomically $ do
     pullCell = Pull $ const $ readTMVar var
   }
 
+previous :: IO (Cell ctx a (Maybe a))
+previous = atomically $ do
+  var <- newTMVar (Nothing, Nothing)
+  return $ Cell {
+    pushCell = Push $ const $ \a -> modifyTMVar var (\(latest, previous) -> (Just a, latest)),
+    pullCell = Pull $ const $ snd <$> readTMVar var
+  }
+
 all :: IO (Cell ctx a [a])
 all = atomically $ do
   var <- newTMVar []
@@ -158,21 +202,27 @@ all = atomically $ do
     pushCell = Push $ const $ modifyTMVar var . (:),
     pullCell = Pull $ const $ readTMVar var
   }
-    where
-      modifyTMVar :: TMVar a -> (a -> a) -> STM ()
-      modifyTMVar var f = do
-        v <- readTMVar var
-        putTMVar var $ f v
+
+-- x :: IO (Cell ctx1 a1 (Maybe a1), Cell ctx2 a2 (Maybe a2), Cell ctx3 a3 [a3])
+-- x :: IO (Cell ctx a (Maybe a1, Maybe a2, [a]))
+-- x = do
+--   return $ (,,) <$> latest <*> previous <*> PushPull.all
+
+-- utility
+modifyTMVar :: TMVar a -> (a -> a) -> STM ()
+modifyTMVar var f = do
+  v <- readTMVar var
+  putTMVar var $ f v
 
 
 -- Runtime
 
 mkPush :: Int -> (a -> IO ()) -> IO (Push ctx a)
-mkPush queueSize io = do
+mkPush queueSize consume = do
     q <- newTBQueueIO (fromIntegral queueSize)
     forkIO $ forever $ do
       a <- atomically $ readTBQueue q
-      io a
+      consume a
     return $ Push $ const $ writeTBQueue q
 
 push :: Exception e => IO ctx -> Push ctx a -> a -> IO (Either e ())
